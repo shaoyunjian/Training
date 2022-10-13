e7dfc78c62015d2d604fdcf7f127e28525c1fd80
shaoyunjian <shaoyunjian@gmail.com>
2022-10-13 17:31:51 +0800

    edit week-4.0. change session to cookie.

---
+++ week-4.1/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, make_response, render_template, redirect, request
+app = Flask(__name__)
+app.secret_key = "anything"
+
+# 首頁
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+# 登入驗證功能
+@app.route("/signin", methods=["POST"])
+def signIn():
+  accountName = request.form["account-name"]
+  password = request.form["password"]
+  if accountName == "test" and password == "test":
+    response = make_response(redirect("/member"))
+    response.set_cookie("loginStatus", "successful")
+    return response
+  else:
+    return redirect("/error?message=請輸入正確的帳號密碼")
+
+# 登入成功
+@app.route("/member")
+def member():
+  if request.cookies["loginStatus"] == "successful": 
+    return render_template("member.html")
+  else:
+    return redirect("/")
+
+# 登入失敗
+@app.route("/error")
+def error():
+  errorMessage = request.args.get("message")
+  return render_template("error.html", message=errorMessage)
+
+# 登出，轉回首頁
+@app.route("/signout")
+def signOut():
+  response = make_response(redirect("/"))
+  response.delete_cookie("loginStatus")
+  response.set_cookie("loginStatus","logout")
+  return response
+
+app.run(port=3000)
