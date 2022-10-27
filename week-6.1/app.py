52e48416cfa19dc1aee38bf0450291640ef2238e
shaoyunjian <shaoyunjian@gmail.com>
2022-10-27 15:06:08 +0800

    Add connection pool

--- week-6.1/app.py
+++ week-6.1/app.py
@@ -12,16 +12,13 @@ app.secret_key = SECRET_KEY
 from mysql.connector.pooling import MySQLConnectionPool
 cnxpool = MySQLConnectionPool(
   pool_name = "mypool",
-  pool_size=10,
+  pool_size=5,
   host="localhost",
   port='3306', 
   user=USER, 
   password=PASSWORD, 
   database="website"
 )
-
-connection = cnxpool.get_connection()
-cursor = connection.cursor()
 
 # Homepage
 @app.route("/")
@@ -31,69 +28,84 @@ def index():
 # Signup
 @app.route("/signup", methods=["POST"])
 def signup():
-  name = request.form["name"]
-  username = request.form["username"]
-  password = request.form["password"]
-  select = "SELECT member.username FROM member WHERE username=%s"
-  values = (username,)
-  cursor.execute(select, values)
-  usernameDB = cursor.fetchone()
-
-  if not name or not username or not password:
-    return redirect(url_for("error", message="請不要有空白"))
-  elif usernameDB == None:
-    insert = "INSERT INTO member(name, username, password) VALUES (%s, %s, %s)"
-    values = (name, username, password)
-    cursor.execute(insert, values)
-    connection.commit()
-    return redirect("/")
-  else:
-    return redirect(url_for("error", message="帳號已經被註冊"))
+  try:
+    cnx1 = cnxpool.get_connection()
+    cursor1 = cnx1.cursor()
+    name = request.form["name"]
+    username = request.form["username"]
+    password = request.form["password"]
+    select = "SELECT member.username FROM member WHERE username=%s"
+    values = (username,)
+    cursor1.execute(select, values)
+    usernameDB = cursor1.fetchone()
+    if not name or not username or not password:
+      # print("Connection db1:", cnx1.connection_id)
+      return redirect(url_for("error", message="請不要有空白"))
+    elif usernameDB == None:
+      insert = "INSERT INTO member(name, username, password) VALUES (%s, %s, %s)"
+      values = (name, username, password)
+      cursor1.execute(insert, values)
+      cnx1.commit()
+      return redirect("/")
+    else:
+      return redirect(url_for("error", message="帳號已經被註冊"))
+  finally:
+    cursor1.close()
+    cnx1.close()
 
 # Login
 @app.route("/signin", methods=["POST"])
 def signin():
-  username = request.form["username"]
-  password = request.form["password"]
-  select = "SELECT * FROM member WHERE username=%s AND password=%s"
-  values = (username, password)
-  cursor.execute(select, values)
-  user = cursor.fetchone()
-  if not username or not password:
-    return redirect(url_for("error", message="請不要有空白"))
-  elif user == None:
-    return redirect("/error?message=帳號或密碼輸入錯誤")
-  else:
-    session['loggedin'] = True
-    session['id'] = user[0]
-    session["name"] = user[1]
-    session["username"] = username
-    session["password"] = password 
-    return redirect("/member")
+  with cnxpool.get_connection() as cnx2:
+    with cnx2.cursor() as cursor2:
+      # print("Connection db2:", cnx2.connection_id)
+      username = request.form["username"]
+      password = request.form["password"]
+      select = "SELECT * FROM member WHERE username=%s AND password=%s"
+      values = (username, password)
+      cursor2.execute(select, values)
+      user = cursor2.fetchone()
+      if not username or not password:
+        return redirect(url_for("error", message="請不要有空白"))
+      elif user == None:
+        return redirect("/error?message=帳號或密碼輸入錯誤")
+      else:
+        session['loggedin'] = True
+        session['id'] = user[0]
+        session["name"] = user[1]
+        session["username"] = username
+        session["password"] = password
+        return redirect("/member")
 
 # Login success
 @app.route("/member")
 def member():
-  if 'loggedin' in session:
-    select_message = ("SELECT member.name, message.content FROM member INNER JOIN message ON member.id = message.member_id ORDER BY message.time DESC")
-    cursor.execute(select_message)
-    messages = cursor.fetchall()
-    return render_template("member.html", name=session["name"], messages=messages)
-  else:
-    return redirect("/")
+  with cnxpool.get_connection() as cnx3:
+    with cnx3.cursor() as cursor3:
+      # print("Connection db3:", cnx3.connection_id)
+      if 'loggedin' in session:
+        select_message = ("SELECT member.name, message.content FROM member INNER JOIN message ON member.id = message.member_id ORDER BY message.time DESC")
+        cursor3.execute(select_message)
+        messages = cursor3.fetchall()
+        return render_template("member.html", name=session["name"], messages=messages)
+      else:
+        return redirect("/")
 
 # Message
 @app.route("/message", methods=["POST"])
 def message():
-  message = request.form["message"]
-  if not message:
-    return redirect("/member")
-  else:
-    insert = "INSERT INTO message(member_id, content) VALUES (%s, %s)"
-    values = (str(session['id']), message)
-    cursor.execute(insert, values)
-    connection.commit()
-    return redirect("/member")
+  with cnxpool.get_connection() as cnx4:
+    with cnx4.cursor() as cursor4:
+      # print("Connection db4:", cnx4.connection_id)
+      message = request.form["message"]
+      if not message:
+        return redirect("/member")
+      else:
+        insert = "INSERT INTO message(member_id, content) VALUES (%s, %s)"
+        values = (str(session['id']), message)
+        cursor4.execute(insert, values)
+        cnx4.commit()
+        return redirect("/member")
 
 # Login failed
 @app.route("/error")
@@ -112,5 +124,3 @@ def signout():
   return redirect("/")
 
 app.run(port=3000)
-cursor.close()
-connection.close()
